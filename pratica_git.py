2c7ad803e88dd405dc2bd7d930a7b765e57f3b9f
HugoMonsalveSalazar <monsalvehugo8@gmail.com>
2021-10-16 08:39:06 -0500

    Nuevo Codigo Funcional
    Cambio del codigo ya funcional

--- pratica_git.py
+++ pratica_git.py
@@ -1,14 +1,12 @@
 def read():
-
     names = []
     with open('./nombres_estudiantes.txt', 'r', encoding='utf-8') as f:
         for line in f:
             names.append(line)
-            print(names)
+        print(names)
 
 
 def write():
-
     animes = ["One piece", "Naruto", "Inuyasha",
               "Shaman King", "El detective Conan"]
     with open('./lista_anime.txt', 'w', encoding='utf-8') as f:
@@ -18,19 +16,15 @@ def write():
 
 
 def insert_anime_console():
-
     continuar = 'y'
-
-
-while continuar == 'y':
-with open('./lista_anime.txt', 'a', encoding='utf-8') as f:
-nuevo_anime = input('Ingresa el nombre del nuevo anime:')
-f.write(nuevo_anime)
-f.write('\n')
-continuar = input("Desea continuar (y/n): ")
-if continuar == 'n':
-print('El programa a finalizado correctamente.')
-
+    while continuar == 'y':
+        with open('./lista_anime.txt', 'a', encoding='utf-8') as f:
+            nuevo_anime = input('Ingresa el nombre del nuevo anime:')
+            f.write(nuevo_anime)
+            f.write('\n')
+            continuar = input("Desea continuar (y/n): ")
+            if continuar == 'n':
+                print('El programa a finalizado correctamente.')
 
 # def run():
 # # read()
